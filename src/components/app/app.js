2498e44719a91c7a29e5ba320cf1f896464dd612
muwahhid <zelem139@gmail.com>
2025-02-18 11:36:04 +0300

    test

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,10 @@
 import { Component } from "react";
 import MovieList from "../movie-list/movie-list";
-import FetchApi from "../fetch-api/fetch-api";
+import { Offline, Online } from "react-detect-offline";
+import { Alert } from "antd";
 import Header from "../header/header";
 
+import "./media.css";
 import "./app.css";
 
 export default class App extends Component {
@@ -15,14 +17,53 @@ export default class App extends Component {
   };
 
   componentDidMount() {
-    <FetchApi />;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YTc0MDk1YTQ5YWNjMjc1ODRmMTVhNzExOWEwNjQ5ZiIsIm5iZiI6MTczOTY1NDI2Mi44NDYwMDAyLCJzdWIiOiI2N2IxMDQ3NmFjMDg4YTQxNDU2YzU1N2EiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.gnVJJMRbg5IxGwgkj0MHQQbS5Zy_kbCIZKcR7-FIKc8",
+      },
+    };
+
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+      options,
+    )
+      .then((response) => {
+        if (!response.ok) throw new Error(`Ошибка: ${response.status}`);
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          movies: data,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          error: {
+            status: true,
+            message: { error },
+          },
+        });
+      });
   }
 
   render() {
     return (
       <main>
         <Header />
-        <MovieList movies={this.state.movies} error={this.state.error} />
+        <Online>
+          <MovieList movies={this.state.movies} error={this.state.error} />
+        </Online>
+        <Offline>
+          <Alert
+            message="Error"
+            description="Нет подключения к интернету!"
+            type="error"
+            showIcon
+          />
+        </Offline>
       </main>
     );
   }
